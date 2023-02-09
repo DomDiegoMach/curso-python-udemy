52aff3a65b3ba9b123c09ec9eabbd16550470239
DomDiegoMach <diegomachado.ene@gmail.com>
2023-02-09 15:48:10 -0300

    feat: aula 12 - Exercício calcular imc com python

---
+++ secao_03/aula12.py
@@ -0,0 +1,9 @@
+nome = "Diego Machado"
+idade = 38
+altura_em_metros = 1.88
+peso_em_kg = 100
+
+imc = peso_em_kg / (altura_em_metros) ** 2
+
+print(f"{nome} tem {idade} anos e {altura_em_metros} de altura")
+print(f"Seu imc é de {imc}")
