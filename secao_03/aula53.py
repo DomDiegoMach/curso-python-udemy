2d75a6d9891330ea04120e39c65a9be2c3e94e88
DomDiegoMach <diegomachado.ene@gmail.com>
2023-02-16 04:06:49 -0300

    feat: aula 53 - Introdução ao enumerate

--- secao_03/aula53.py
+++ secao_03/aula53.py
@@ -5,15 +5,16 @@ enumerate - enumera iteráveis (índices)
 lista = ["Maria", "Helena", "Luiz"]
 lista.append("João")
 
-for indice, nome in enumerate(lista):
-    print(indice, nome, lista[indice])
+# for indice, nome in enumerate(lista, start=0):
+#     # for indice, nome in enumerate(lista):
+#     print(indice, nome, lista[indice])
 
-# for item in enumerate(lista):
+# for item in enumerate(lista, start=10):
 #     indice, nome = item
 #     print(indice, nome)
 
 
-# for tupla_enumerada in enumerate(lista):
-#     print('FOR da tupla:')
-#     for valor in tupla_enumerada:
-#         print(f'\t{valor}')
+for tupla_enumerada in enumerate(lista):
+    print("FOR da tupla:")
+    for valor in tupla_enumerada:
+        print(f"\t{valor}")
