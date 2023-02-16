c839e2d8e81f24ab76a2f7d7749cdbfddf934d46
DomDiegoMach <diegomachado.ene@gmail.com>
2023-02-16 00:10:39 -0300

    feat: aula 48 -Inserindo elementos na lista com insert

--- secao_03/aula48.py
+++ secao_03/aula48.py
@@ -7,16 +7,49 @@ Métodos úteis: append, insert, pop, del, clear, extend, +
 """
 #        +01234
 #        -54321
+"""
 string = "ABCDE"  # 5 caracteres (len)
 lista = []
 # lista = list()
 print("1", bool([]))  # falsy
 print("2", lista, type(lista))
+"""
 
 #        0    1      2              3    4
 #       -5   -4     -3             -2   -1
+""" - Parte 01 - Aqui
 lista = [123, True, "Luiz Otávio", 1.2, []]
 lista[-3] = "Maria"
 print("3", lista)
 print("4", lista[2], type(lista[2]))
 print(lista[4])
+"""
+
+"""" - Parte 02 - Aqui
+#        0   1   2   3
+lista = [10, 20, 30, 40]
+lista.append('Luiz')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(100, 5)
+print(lista[4])
+"""
+
+""" - Parte 03 - Aqui """
+lista = [10, 20, 30, 40]
+print(lista)
+lista.append("Luiz")
+print(lista)
+nome = lista.pop()
+print(lista)
+lista.append(1233)
+print(lista)
+del lista[-1]
+print(lista)
+lista.clear()
+print(lista)
+lista.insert(100, 5)
+print(lista)
+print(lista[0])
