fb55f498734b3ddef4bca3e57472a861feea42dc
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-04 10:41:35 -0300

    feat: aula 60 - Operações ternárias com Python

--- secao_03/aula60.py
+++ secao_03/aula60.py
@@ -5,8 +5,8 @@ Operação ternária (condicional de uma linha)
 # condicao = 10 == 11
 # variavel = 'Valor' if condicao else 'Outro valor'
 # print(variavel)
-# digito = 9  # > 9 = 0
-# novo_digito = digito if digito <= 9 else 0
+digito = 4  # > 9 = 0
+novo_digito = digito if digito <= 5 else 0
 # novo_digito = 0 if digito > 9 else digito
-# print(novo_digito)
-print("Valor" if False else "Outro valor" if False else "Fim")
+print(novo_digito)
+# print("Valor" if False else "Outro valor" if False else "Fim")
