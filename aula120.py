d1d516a22e1d4e48d1d4a3959caad64ac4aae077
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-05 00:40:45 -0300

    feat: aula 120 - Introdução a POO - Python

--- aula120.py
+++ aula120.py
@@ -1,32 +1,29 @@
-# class - Classes são moldes para criar novos objetos
+# Introdução ao método __init__ (inicializador de atributos)
 # As classes geram novos objetos (instâncias) que
 # podem ter seus próprios atributos e métodos.
 # Os objetos gerados pela classe podem usar seus dados
 # internos para realizar várias ações.
 # Por convenção, usamos PascalCase para nomes de
 # classes.
-
-# string = "Luiz"  # str
+# string = 'Luiz'  # str
 # print(string.upper())
 # print(isinstance(string, str))
-
-
 class Pessoa:
-    ...
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
 
 p1 = Pessoa("Luiz", "Otávio")
-print(p1)
+# p1.nome = 'Luiz'
+# p1.sobrenome = 'Otávio'
 
-# p1.nome = "Luiz"
-# p1.sobrenome = "Otávio"
+p2 = Pessoa("Maria", "Joana")
+# p2.nome = 'Maria'
+# p2.sobrenome = 'Joana'
 
-# p2 = Pessoa("Maria", "Joana")
-# p2.nome = "Maria"
-# p2.sobrenome = "Joana"
+print(p1.nome)
+print(p1.sobrenome)
 
-# print(p1.nome)
-# print(p1.sobrenome)
-
-# print(p2.nome)
-# print(p2.sobrenome)
+print(p2.nome)
+print(p2.sobrenome)
