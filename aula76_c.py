ba66f5df10e67b88fc2f23e54f786083d3e58ac1
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-09 12:55:09 -0300

    feat: aula 76_c - (Parte 1) Métodos úteis nos dicionários Python (dict)

--- aula76_c.py
+++ aula76_c.py
@@ -12,18 +12,19 @@
 pessoa = {
     "nome": "Luiz Otávio",
     "sobrenome": "Miranda",
-    "idade": 900,
+    # "idade": 900,
 }
 
 pessoa.setdefault("idade", 0)
 print(pessoa["idade"])
 # print(len(pessoa))
 # print(list(pessoa.keys()))
+# print(tuple(pessoa.keys()))
 # print(list(pessoa.values()))
 # print(list(pessoa.items()))
 
 # for valor in pessoa.values():
 #     print(valor)
 
-# for chave, valor in pessoa.items():
-#     print(chave, valor)
+for chave, valor in pessoa.items():
+    print(chave, valor)
