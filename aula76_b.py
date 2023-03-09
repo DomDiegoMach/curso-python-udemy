9240c2b43aee3a71de990b80b9989f4dc7176a12
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-09 10:55:22 -0300

    feat: aula 76_b - Manipulando chaves e valores em dicionários

---
+++ aula76_b.py
@@ -0,0 +1,28 @@
+# Manipulando chaves e valores em dicionários
+pessoa = {}
+
+##
+##
+
+chave = "nome"
+
+pessoa[chave] = "Luiz Otávio"
+pessoa["sobrenome"] = "Miranda"
+pessoa["idade"] = 36
+
+print(pessoa)
+print(pessoa[chave])
+
+pessoa[chave] = "Maria"
+
+del pessoa["sobrenome"]
+print(pessoa)
+print(pessoa["nome"])
+
+print(pessoa.get("sobrenome"))
+if pessoa.get("sobrenome") is None:
+    print("NÃO EXISTE")
+else:
+    print(pessoa["sobrenome"])
+
+print("ISSO Não vai")
