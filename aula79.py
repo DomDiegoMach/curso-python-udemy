538f4eca941bb2c8accc94e6da0ab5c549f651b8
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-11 01:00:54 -0300

    feat: aula 79 - Exemplo de uso do tipo set

--- aula79.py
+++ aula79.py
@@ -1,8 +1,13 @@
 # Exemplo de uso dos sets
 letras = set()
 while True:
-    letra = input("Digite: ")
-    letras.add(letra.lower())
+    letra = str(input("Digite: "))
+    if len(letra) == 1:
+        letras.add(letra.lower())
+    elif letra in letras:
+        print("Letra repetida")
+    else:
+        print("Digite apenas uma letra")
 
     if "l" in letras:
         print("PARABÉNS")
