bfddc55b039f99ca8e402119d8309082770a89b6
DomDiegoMach <diegomachado.ene@gmail.com>
2023-03-21 16:28:14 -0300

    feat: aula 82 - Funções lambda complexas (para entendimento)

--- aula82.py
+++ aula82.py
@@ -16,8 +16,6 @@ def executa(funcao, *args):
 duplica = executa(lambda m: lambda n: n * m, 2)
 print(duplica(2))
 
-print(
-    executa(lambda x, y: x + y, 2, 3),
-)
+print(executa(lambda x, y: x + y, 2, 3))
 
 print(executa(lambda *args: sum(args), 1, 2, 3, 4, 5, 6, 7))
